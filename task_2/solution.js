704b82adf788a3bea7cb8ff718ab3a0a949824c9
Masmv <107849591+Masmv@users.noreply.github.com>
2022-07-03 22:32:47 +0500

    Расчет доставки

--- task_2/solution.js
+++ task_2/solution.js
@@ -5,17 +5,17 @@ function calcShipping(sum, min, shipping) {
 
     // Задание №2.1. Рассчитать доставку
 
-    // создайте переменную shippingSum
+    let shippingSum               // создайте переменную shippingSum
 
-    // если productsSum равно 0,
-    // то shippingSum присвоить значение 0
-
-    // если productsSum Больше или равна freeShippingMinSum,
-    // то shippingSum присвоить значение 0
-
-    // если productsSum больше 0 и меньше freeShippingMinSum,
-    // то shippingSum присвоить значение shippingPrice
-
+    if (productsSum == 0){        // если productsSum равно 0,
+        shippingSum == 0;         // то shippingSum присвоить значение 0
+    }
+    
+    if productsSum >= freeShippingMinSum    // если productsSum Больше или равна freeShippingMinSum,
+       shippingSum == 0;                    // то shippingSum присвоить значение 0
+    }else {productsSum == freeShippingMinSum // если productsSum больше 0 и меньше freeShippingMinSum,
+                                             // то shippingSum присвоить значение shippingPrice
+    }
     // Конец решения задания №2.1.
 
     return shippingSum;
