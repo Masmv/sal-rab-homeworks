5e331ef1f103c3903dd4853c75efccc9e4840a60
Masmv <107849591+Masmv@users.noreply.github.com>
2022-07-03 22:41:09 +0500

    изменена логика

--- task_2/solution.js
+++ task_2/solution.js
@@ -13,8 +13,10 @@ function calcShipping(sum, min, shipping) {
     
     if (productsSum >= freeShippingMinSum){    // если productsSum Больше или равна freeShippingMinSum,
        shippingSum == 0;                    // то shippingSum присвоить значение 0
-    }else {productsSum == freeShippingMinSum // если productsSum больше 0 и меньше freeShippingMinSum,
-                                             // то shippingSum присвоить значение shippingPrice
+    }
+    
+    if (productsSum > 0) $$ (productsSum < freeShippingMinSum){  // если productsSum больше 0 и меньше freeShippingMinSum,
+        shippingSum == shipping;                                     // то shippingSum присвоить значение shippingPrice
     }
     // Конец решения задания №2.1.
 
