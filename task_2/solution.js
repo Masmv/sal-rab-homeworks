f4a3dae5f0879086e3a5faf00b86280b160d9957
Masmv <107849591+Masmv@users.noreply.github.com>
2022-07-04 09:55:55 +0500

    1

--- task_2/solution.js
+++ task_2/solution.js
@@ -5,7 +5,7 @@ function calcShipping(sum, min, shipping) {
     // Задание №2.1. Рассчитать доставку
 
 let shippingSum;
-let shippingPrice = 200;
+let shippingPrice = min;
 
 if (productsSum == 0) {
 shippingSum = 0;
