3c1fb1bcf36d0237b28afaded168b14cd0e51399
Masmv <107849591+Masmv@users.noreply.github.com>
2022-07-04 09:24:46 +0500

    correct

--- task_2/solution.js
+++ task_2/solution.js
@@ -8,10 +8,10 @@ function calcShipping(sum, min, shipping) {
 if (productsSum == 0) {
 shippingSum = 0;
 } 
-else if (productsSum >= freeShippingMinSum){
+else if (productsSum >= freeShippingMinSum) {
 shippingSum = 0;
 } 
-else if ((productsSum > 0) && (productsSum < freeShippingMinSum)) {
+else if (productsSum > 0 && productsSum < freeShippingMinSum) {
 shippingSum = shippingPrice;
 }
                                         
