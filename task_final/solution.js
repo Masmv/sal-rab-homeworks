025becb2ed8b2578f8e2b83d886384f82dc6bc58
Masmv <107849591+Masmv@users.noreply.github.com>
2022-07-13 21:00:12 +0500

    test

--- task_final/solution.js
+++ task_final/solution.js
@@ -10,7 +10,7 @@ function sendRequest(name, phone, address, goods, sum) {
         data.goods.push({title: goods[i].title, count: goods[i].count});
 }
     
-    data.order.address = address{street: 'ул.', house: 'дом', entrance: 'подъезд', floor: 'этаж', flat: 'кв'};
+    data.order.address = {street: 'ул. ' + address.street, house: 'дом', entrance: 'подъезд', floor: 'этаж', flat: 'кв'};
     
     //data.order.sum = name + phone + address + goods + sum;
 
