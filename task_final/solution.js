1ef4f94de28c57f6a9db32954fa7628874653a15
Masmv <107849591+Masmv@users.noreply.github.com>
2022-07-13 21:14:51 +0500

    test

--- task_final/solution.js
+++ task_final/solution.js
@@ -1,6 +1,6 @@
 function sendRequest(name, phone, address, goods, sum) {
     let data = {client: name + " " + phone, 
-                order: {addressNew, sum}, 
+                order: {address, sum}, 
                 goods: []
         }
 
@@ -10,10 +10,11 @@ function sendRequest(name, phone, address, goods, sum) {
         data.goods.push({title: goods[i].title, count: goods[i].count});
 }
     
-    data.order.address = {street, house, entrance, floor, flat}
+    data.order.address = {'ул. ' + address.street, 'дом ' + address.house, address.entrance + ' подъезд', address.floor + ' этаж' , 'кв' + address.flat};
+    //{street, house, entrance, floor, flat}
     //{'ул. ' + address.street, 'дом ' + address.house, address.entrance + ' подъезд', address.floor + ' этаж' , 'кв' + address.flat};
     
-    let adressNew = {'ул. ' + address.street, 'дом ' + address.house, address.entrance + ' подъезд', address.floor + ' этаж' , 'кв' + address.flat};
+    //adress = {'ул. ' + address.street, 'дом ' + address.house, address.entrance + ' подъезд', address.floor + ' этаж' , 'кв' + address.flat};
     //data.order.sum = name + phone + address + goods + sum;
 
     //data.client = 'Иван';
